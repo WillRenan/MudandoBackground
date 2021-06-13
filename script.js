659fdace21f958176a5b78cdabdf2b379f5e1edc
WillRenan <willdesign,will@gmail.com>
2021-06-13 02:13:19 -0300

    Finalizado

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function changeColor(getColor){
+    let bg = document.querySelector('.bg');
+    let selectCor = getColor.value;
+    let text = getColor.options[getColor.selectedIndex]
+                .text;
+
+    bg.style.background = selectCor;
+    bg.innerHTML = text;
+}
