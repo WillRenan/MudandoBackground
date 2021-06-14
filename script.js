2227e0d362b457e959ec03a749dc726fd8e0ffbb
WillRenan <willdesign,will@gmail.com>
2021-06-14 00:11:10 -0300

    add hover

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function changeColor(getColor){
-    let bg = document.querySelector('.bg');
+    let bg = document.querySelector('.bg ');
     let selectCor = getColor.value;
     let text = getColor.options[getColor.selectedIndex]
                 .text;
